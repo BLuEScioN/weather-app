f0825ea6697174e2396e2a3e021117a048bc4884
Nicholas Barnett <barnettn26@gmail.com>
2019-07-25 21:09:13 -0700

    added mainpage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-app-boilerplate
+
+# How to run locally
---
+++ src/scripts/MainPage.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export class MainPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <p>This is my new react app</p>
+                <p>This is my new react app</p>
+            </>
+        );
+    }
+
+}
