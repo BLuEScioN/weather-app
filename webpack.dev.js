c81a36f321b57cc9983ada2d5c328215c91b38b5
Nicholas Barnett <barnettn26@gmail.com>
2019-07-28 21:16:58 -0700

    scss

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,78 +1,78 @@
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isDevelopment = process.env.NODE_ENV === 'development'
+module.exports = env => {
+	const isDevelopment = env.NODE_ENV === 'development';
 
-module.exports = {
-	entry: {
-		'react-app': path.join(__dirname, '/src/index.js')
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].js'
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx', '.css', '.less']
-	},
-	module: {
-		rules: [
-				{
-					test: /\.(ts|tsx)?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/
-				}, 
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env'],
-							plugins: ['@babel/plugin-proposal-object-rest-spread'],
-							cacheDirectory: true
+	return {
+		mode: 'development',
+		entry: {
+			'react-app': path.join(__dirname, '/src/index.js')
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: '[name].js'
+		},
+		resolve: {
+			extensions: ['.js', '.jsx', '.ts', '.tsx', '.css', '.less']
+		},
+		module: {
+			rules: [
+					{
+						test: /\.(ts|tsx)?$/,
+						use: 'ts-loader',
+						exclude: /node_modules/
+					}, 
+					{
+						test: /\.(js|jsx)$/,
+						exclude: /node_modules/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-env'],
+								plugins: ['@babel/plugin-proposal-object-rest-spread'],
+								cacheDirectory: true
+							}
 						}
+					},
+					{
+						test: /\.css$/,
+						loader: [
+							isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+							'css-loader',
+						]
+					},
+					{
+						test: /\.s(a|c)ss$/,
+						// exclude: /\.module.(s(a|c)ss)$/,
+						loader: [
+							isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+							'css-loader',
+							{
+								loader: 'sass-loader',
+								options: {
+								sourceMap: isDevelopment
+								}
+							}
+						]
 					}
-				},
-				 {
-					test: /\.css$/,
-					loader: [
-						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-						'css-loader',
-					]
-				},
-				{
-					test: /\.s(a|c)ss$/,
-					// exclude: /\.module.(s(a|c)ss)$/,
-					loader: [
-					  isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-					  'css-loader',
-					  {
-						loader: 'sass-loader',
-						options: {
-						  sourceMap: isDevelopment
-						}
-					  }
-					]
-				  }
-		]
-	}, 
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-      		chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-		}),
-		// new ExtractTextPlugin('[name].css'),
-		new CopyWebpackPlugin([{
-			from: './src/html'
-		}])
-		// new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		contentBase: path.join(__dirname, 'dist/react-app.js'),
-		compress: true,
-		port: 8080,
-		historyApiFallback: true
+			]
+		}, 
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+				chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+			}),
+			new CopyWebpackPlugin([{
+				from: './src/html'
+			}]),
+		],
+		devServer: {
+			contentBase: path.join(__dirname, 'dist/react-app.js'),
+			compress: true,
+			port: 8080,
+			historyApiFallback: true
+		}
 	}
 }
